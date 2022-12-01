aaae3c57db93b2f512e744fe659091cbc57cb1d6
whitslar <whitslar.1@gmail.com>
2022-12-01 10:41:26 -0500

    Add transform_keys! that was intro'd in Ruby 2.5

--- lib/dip/ext/hash.rb
+++ lib/dip/ext/hash.rb
@@ -15,6 +15,23 @@ module ActiveSupportHashHelpers
 
       self
     end
+    
+    def transform_keys
+      return enum_for(:transform_keys) { size } unless block_given?
+      result = {}
+      each_key do |key|
+        result[yield(key)] = self[key]
+      end
+      result
+    end
+    
+    def transform_keys!
+      return enum_for(:transform_keys!) { size } unless block_given?
+      keys.each do |key|
+        self[yield(key)] = delete(key)
+      end
+      self
+    end
 
     def deep_merge(other_hash, &block)
       dup.deep_merge!(other_hash, &block)
